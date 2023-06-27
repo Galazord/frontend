0fd903abdbc949bb3fbe5ea934841207fdaf01fd
Alberto <galbarack@gmail.com>
2023-06-27 13:01:12 +0200

    commit-V1-save-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'config',
+        component: ConfigurationComponent
+      }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
